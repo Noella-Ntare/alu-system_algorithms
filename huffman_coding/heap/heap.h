d64798324072673c021c46dc974351862c2e9c4b
Noella-Ntare <n.ntare@alustudent.com>
2026-05-29 08:35:49 +0200

    huffman

--- huffman_coding/heap/heap.h
+++ huffman_coding/heap/heap.h
@@ -1,28 +1,43 @@
-#include "heap.h"
+#ifndef HEAP_H
+#define HEAP_H
+
+#include <stdlib.h>
 
 /**
- * binary_tree_node - program that creates a new binary tree node
- * @parent: a pointer to the parent node of the node to be created
- * @data: the data to be stored in the node
- * Return: a pointer to the created node, or NULL if it fails
- */
-
-binary_tree_node_t *binary_tree_node(binary_tree_node_t *parent, void *data)
+* struct binary_tree_node_s - Binary tree node data structure
+*
+* @data: Data stored in a node
+* @left: Pointer to the left child
+* @right: Pointer to the right child
+* @parent: Pointer to the parent node
+*/
+typedef struct binary_tree_node_s
 {
-	binary_tree_node_t *new_node = NULL;
+	void *data;
 
-	if (data == NULL)
-		return (NULL);
+	struct binary_tree_node_s *left;
+	struct binary_tree_node_s *right;
+	struct binary_tree_node_s *parent;
+} binary_tree_node_t;
 
-	new_node = malloc(sizeof(binary_tree_node_t));
+/**
+* struct heap_s - Heap data structure
+*
+* @size: Size of the heap (number of nodes)
+* @data_cmp: Function to compare two nodes data
+* @root: Pointer to the root node of the heap
+*/
+typedef struct heap_s
+{
+	size_t size;
+	int (*data_cmp)(void *, void *);
+	binary_tree_node_t *root;
+} heap_t;
 
-	if (new_node == NULL)
-		return (NULL);
+heap_t *heap_create(int (*data_cmp)(void *, void *));
+binary_tree_node_t *binary_tree_node(binary_tree_node_t *parent, void *data);
+binary_tree_node_t *heap_insert(heap_t *heap, void *data);
+void *heap_extract(heap_t *heap);
+void heap_delete(heap_t *heap, void (*free_data)(void *));
 
-	new_node->data = data;
-	new_node->left = NULL;
-	new_node->right = NULL;
-	new_node->parent = parent;
-
-	return (new_node);
-}
+#endif /*HEAP_H*/
